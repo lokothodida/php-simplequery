482d10d576caa6665a492ead6f6548d276132497
Lawrence Okoth-Odida <lawrence.okothodida@gmail.com>
2014-09-05 19:54:56 +0100

    Add constructor, getField method and start building the query method

--- php-simplequery.php
+++ php-simplequery.php
@@ -9,8 +9,29 @@
  */
 class SimpleQuery {
   /** properties */
+  protected $items;
+
   /** public methods */
+  // Constructor
+  public function __construct($items) {
+    $this->items = $items;
+  }
+
+  // Run the query
+  public function query($query, $sort = array(), $limit = 0) {
+    $items = $this->items;
+
+    // ...
+
+    return $items;
+  }
+
   /** protected methods */
+  // Get a field from an item
+  protected function getField($item, $field) {
+    return $item[$field];
+  }
+
   /** private methods */
 }
 
