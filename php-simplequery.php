52dbecaeeb6645c8088c2bd754b344f78c387ed4
Lawrence Okoth-Odida <lawrence.okothodida@gmail.com>
2014-09-05 19:51:44 +0100

    Add meta info/initial comments

--- php-simplequery.php
+++ php-simplequery.php
@@ -1,7 +1,17 @@
 <?php
 
+/**
+ * Title:         PHP SimpleQuery
+ * Version:       0.1
+ * Description:   Perform queries on PHP arrays
+ * Author:        Lawrence Okoth-Odida
+ * Documentation: https://github.com/lokothodida/wiki/
+ */
 class SimpleQuery {
-  // ...
+  /** properties */
+  /** public methods */
+  /** protected methods */
+  /** private methods */
 }
 
 ?>
